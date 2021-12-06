45ed89f5eabe0b71a508e269761167429142d32e
munkh-erdene <odforme@gmail.com>
2021-12-07 01:54:17 +0800

    Update ihc_time_lock.sol

--- ihc_time_lock.sol
+++ ihc_time_lock.sol
@@ -7,21 +7,20 @@ contract IHC_TIME_LOCK {
     address public ihcTokenAddress;
 
     constructor(address payable _owner, uint _daysAfter) public payable {
+        require(_owner != address(0),"_owner: zero address!");
         end = block.timestamp + (_daysAfter * 1 days);
         owner = _owner;
         ihcTokenAddress = 0x86a53fcd199212FEa44FA7e16EB1f28812be911D;
     }
 
-    function deposit(uint amount) external {
-        IHC(ihcTokenAddress).transferFrom(msg.sender, address(this), amount);
+    function deposit(uint amount) external returns(bool) {
+        return IHC(ihcTokenAddress).transferFrom(msg.sender, address(this), amount);
     }
     
     function withdraw(uint amount) external returns(bool) {
         require(msg.sender == owner, 'only owner');
         require(block.timestamp >= end, 'too early');
-        IHC(ihcTokenAddress).transfer(owner, amount);
-        
-        return true;
+        return IHC(ihcTokenAddress).transfer(owner, amount);
     }
     
     function getEndOfTime() external view returns (uint256) {
